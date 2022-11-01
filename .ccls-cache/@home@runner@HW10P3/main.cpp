a3c44c93c7a484ee8b3eb3c3993fb37e77c4207c
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-11-01 04:42:16 +0000

    added else if for final cout

--- .ccls-cache/@home@runner@HW10P3/main.cpp
+++ .ccls-cache/@home@runner@HW10P3/main.cpp
@@ -6,23 +6,24 @@ int main() {
 
   int numNums;
   int temp;
-
+  
   cin >> numNums;
   int numbers[numNums];
-
+  
   for (int i = 0; i < numNums; i++){
     cin >> numbers[i];
-    }
     
-  for (int i = 0; i < numNums; i++){
-      if(i == 0){
-        cout << temp;
-        temp = numbers[0];
     }
 
-
-
-  
-}
-
-}
+  for (int i = 0; i < numNums; i++){
+    if(i == 0){
+      temp = numbers[0];
+      cout << temp;
+      
+    }
+    else if(temp < numbers[i])
+    cout << numbers[i];
+    temp = numbers[i];
+    
+  }
+  }
