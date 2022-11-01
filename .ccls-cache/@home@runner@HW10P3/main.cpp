e3d5df590da406f7a612ed00fa1acbd6a1520890
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-11-01 15:57:56 +0000

    Added function to code

--- .ccls-cache/@home@runner@HW10P3/main.cpp
+++ .ccls-cache/@home@runner@HW10P3/main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main() {
 
-  int numNums;
-  int temp;
-  
-  cin >> numNums;
-  int numbers[numNums];
-  
+void numArray(int numbers[], int temp, int numNums){
   for (int i = 0; i < numNums; i++){
     cin >> numbers[i];
     
@@ -26,4 +21,17 @@ int main() {
     temp = numbers[i];
     
   }
+  }
+
+
+int main() {
+
+  int numNums;
+  int temp;
+  
+  cin >> numNums;
+  int numbers[numNums];
+  
+  numArray(numbers, temp, numNums);
+    
   }
