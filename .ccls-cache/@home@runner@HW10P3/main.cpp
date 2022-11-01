2cfde36e9563bca981941057066cac37532a6083
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-11-01 04:04:40 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW10P3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int numNums;
+  int temp;
+  int numbers[] = {};
+
+  for (int i = 0; i < numNums; i++){
+  
+    if(i == 0){
+      cout << temp;
+      temp = numbers[0];
+    }
+    if()
+    
+  }
+
+
+  
+}
